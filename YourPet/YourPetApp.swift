cdd5ebb842132edf36824f2995b46eac7c3170ae
Martyna <mieczkowska.maartyna@gmail.com>
2024-10-29 22:54:19 +0100

    Initial Commit

---
+++ YourPet/YourPetApp.swift
@@ -0,0 +1,17 @@
+//
+//  YourPetApp.swift
+//  YourPet
+//
+//  Created by Martyna Mieczkowska on 29/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct YourPetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
